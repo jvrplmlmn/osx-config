365452e3397ffd201432b11a22e9b16f2cff4c09
Javier Palomo Almena <javier.palomo@weather.com>
2016-10-23 17:43:37 +0200

    Atom: Language support (multiple) and Golang plugins

--- config-atom.sh
+++ config-atom.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
+# This can be generated with:
+#  apm list --packages --installed --bare | grep . | cut -d"@" -f 1 | awk '{ print "apm install " $0 }'
+
+apm install language-chef
+apm install language-docker
+apm install language-groovy
+apm install language-hcl
 apm install language-terraform
+apm install linter
+apm install linter-flake8
+apm install multi-cursor
+apm install tabs-to-spaces
