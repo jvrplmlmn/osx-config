fc3ed74df94eff067394c1ac1c66f4ed82a7ee9b
Javier Palomo Almena <javier.palomo@weather.com>
2016-11-03 10:15:58 +0100

    Update Casks. Include README on how to generate the list

--- install-casks.sh
+++ install-casks.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 
+# This can be generated with:
+#  brew cask list -1 | awk '{ print "brew cask install " $0 }'
+
+brew cask install adobe-reader
+brew cask install amethyst
 brew cask install atom
 brew cask install chefdk
 brew cask install docker
+brew cask install flux
+brew cask install google-chrome
+brew cask install iterm2
 brew cask install java
-
-brew cask install virtualbox
-brew cask install vagrant
-
-brew cask install adobe-reader
+brew cask install kube-solo
+brew cask install slack
 brew cask install the-unarchiver
+brew cask install vagrant
+brew cask install virtualbox
+brew cask install vlc
