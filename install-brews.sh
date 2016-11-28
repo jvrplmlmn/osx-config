bf38798bb4afc32510185500a64068d1d296d307
Javier Palomo Almena <javier.palomo@weather.com>
2016-11-28 16:46:22 +0100

    Order the list of installed brews

--- install-brews.sh
+++ install-brews.sh
@@ -5,21 +5,24 @@
 
 brew update
 
-brew install python
-brew install flake8
-brew install tree
-brew install watch
-
-brew install jq
-brew install httpie
-
-brew install git
-brew install hub
-
 brew install awscli
-
-brew install rbenv
-
+brew install binutils
 brew install cheat
+brew install coreutils
+brew install flake8
+brew install git
+brew install glide
+brew install go
+brew install httpie
+brew install hub
+brew install jq
 brew install kubectl
 brew install nmap
+brew install pre-commit
+brew install python
+brew install python3
+brew install rbenv
+brew install tree
+brew install watch
+brew install youtube-dl
+brew install zsh
