27dc2b9033620b34cd3d3c586d665197463173d1
Javier Palomo Almena <javier.palomo.almena@gmail.com>
2016-12-02 22:57:11 +0100

    Tap caskroom/fonts and install several of them (fontawesome, roboto, ...)

---
+++ install-fonts.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Caskroom-fonts is a Homebrew Tap which allows you to use
+# the same friendly Homebrew-style CLI workflow for the
+# administration of binary font files on your Mac.
+brew tap caskroom/fonts
+
+brew cask install font-fontawesome
+brew cask install font-roboto
+brew cask install font-source-sans-pro
