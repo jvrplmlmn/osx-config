e3506557e55fe0b90df91da2618001817cc56b69
Javier Palomo <javier.palomo.almena@gmail.com>
2019-12-31 14:40:46 +0100

    Remove installation of obsolete brews

--- install-brews.sh
+++ install-brews.sh
@@ -5,37 +5,21 @@
 
 brew update
 
-brew install awscli
 brew install bash
 brew install binutils
-brew install cheat
-brew install chruby
 brew install coreutils
-brew install elixir
-brew install flake8
 brew install git
-brew install glide
 brew install gnupg
-brew install go
-brew install gradle
 brew install graphviz
 brew install htop
 brew install httpie
 brew install hub
 brew install jq
-brew install kubernetes-cli
 brew install nmap
-brew install node.js
-brew install pre-commit
-brew install python
 brew install python3
-brew install redis
-brew install ruby-install
-brew install rust
 brew install telnet
 brew install tldr
 brew install tree
 brew install watch
 brew install wget
-brew install youtube-dl
 brew install zsh
