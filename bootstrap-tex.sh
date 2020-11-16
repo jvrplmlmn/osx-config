4b0ac3d0e568d73b9cf25e879f2a716d6f19a134
Javier Palomo <javier.palomo.almena@gmail.com>
2020-11-16 19:02:18 +0100

    Shellcheck codebase

--- bootstrap-tex.sh
+++ bootstrap-tex.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-ME=`id -un`
+ME=$(id -un)
 TEX_PATH="/usr/local/texlive"
 # Assumes that 'stat' is the GNU Coreutils version
-TEX_OWNER=`stat -c '%U' ${TEX_PATH}`
+TEX_OWNER=$(stat -c '%U' ${TEX_PATH})
 
 if [[ "$TEX_OWNER" -ne "$ME" ]]; then
   echo "Change ownership of TexLive (${TEX_PATH}) to ${ME}, so we can use 'tlmgr'"
   echo "Requires sudo"
-  sudo chown -R ${ME}:admin ${TEX_PATH}
+  sudo chown -R "${ME}":admin ${TEX_PATH}
 fi
 
 # Update LaTex manager
@@ -32,5 +32,5 @@ TEX_PACKAGES=(
 
 for PACKAGE in ${TEX_PACKAGES[*]}
 do
-  tlmgr install ${PACKAGE}
+  tlmgr install "${PACKAGE}"
 done
