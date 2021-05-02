bf073a4c859ef5ea90e0d92c7b9a8e52ecacf5b4
Javier Palomo <javier.palomo.almena@gmail.com>
2021-05-02 13:53:38 +0200

    Update install-brews.sh

--- install-brews.sh
+++ install-brews.sh
@@ -6,20 +6,33 @@
 brew update
 
 brew install bash
+brew install bat
 brew install binutils
 brew install coreutils
+brew install curl
+brew install diffutils
+brew install fzf
 brew install git
+brew install gnu-sed
 brew install gnupg
+brew install go-jsonnet
 brew install graphviz
+brew install grep
+brew install helm
 brew install htop
 brew install httpie
 brew install hub
 brew install jq
+brew install k9s
 brew install nmap
+brew install parallel
 brew install python3
+brew install shellcheck
 brew install telnet
 brew install tldr
 brew install tree
+brew install vim
 brew install watch
 brew install wget
+brew install yq
 brew install zsh
